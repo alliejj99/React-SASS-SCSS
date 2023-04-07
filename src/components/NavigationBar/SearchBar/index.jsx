05d17009933830ddc6bab32a50822c791f08a1d5
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-07 22:14:05 +0900

    useWindowReSize Coustom Hooks 생성

--- src/components/NavigationBar/SearchBar/index.jsx
+++ src/components/NavigationBar/SearchBar/index.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { ImSearch as SearchIcon } from "react-icons/im";
 import { MdKeyboardVoice as VoiceIcon } from "react-icons/md";
+import useWindowSize from "../../../helpers/useWindowSize";
 
 const SearchBar = () => {
+  const { width } = useWindowSize(); // windowSize 안의 width
+
   return (
-    <div className={`SearchBar`}>
-      <form>
-        <input type="text" name="search" placeholder="Search" />
-        <button type="submit">
+    <div className={`SearchBar ${width <= 640 ? "smallSearch" : ""}`}>
+      {width > 640 ? (
+        <form>
+          <input type="text" name="search" placeholder="Search" />
+          <button type="submit">
+            <SearchIcon
+              size={20}
+              data-tooltip-content="Search"
+              data-tooltip-id="navbar"
+            />
+          </button>
+        </form>
+      ) : (
+        <button className="icon-conatiner searchIcon">
           <SearchIcon
             size={20}
             data-tooltip-content="Search"
             data-tooltip-id="navbar"
           />
         </button>
-      </form>
+      )}
 
       <button className="icon-container voiceIcon">
         <VoiceIcon
