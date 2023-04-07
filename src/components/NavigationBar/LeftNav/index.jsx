f2029c3e0a813978f13095b3143761f40bb1d6f0
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-07 22:00:32 +0900

    nav 스타일링

--- src/components/NavigationBar/LeftNav/index.jsx
+++ src/components/NavigationBar/LeftNav/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoMenu as Menu } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import logo from "../../../assets/logo.svg";
+import logo from "../../../assets/logo.png";
 
 const LeftNav = () => {
   return (
