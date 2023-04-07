c0d98b109e7e9ad42db236ff6d31ddc75de9861d
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-07 20:54:05 +0900

    React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useCallback } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import SearchedVideosPage from "./pages/SearchedVideosPage";
+import VideoPage from "./pages/VideoPage";
+import NavigationBar from "./components/NavigationBar";
+import SideBar from "./components/SideBar";
+
+const Layout = useCallback(() => {
+  return (
+    <React.Fragment>
+      <NavigationBar />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
+    </React.Fragment>
+  );
+}, []);
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/results/:input" element={<SearchedVideosPage />} />
+          <Route path="/video/:videoId" element={<VideoPage />} />
+        </Route>
+      </Routes>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SideBar/SmallBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SmallBar = () => {
+  return <div>SmallBar</div>;
+};
+
+export default SmallBar;
