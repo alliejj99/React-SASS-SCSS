4d9e488de73a2c974b955a86f1e0c203196445c3
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-10 22:28:43 +0900

    비디오 데이터 가져오기

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { getVideoInfo } from "../../helpers/fetchingData";
 
 const MainPage = () => {
-  return (
-    <div>MainPage</div>
-  )
-}
+  // const [mainVideos, setMainVideos] = useState([]); // 정보를 담아줄 state
 
-export default MainPage
+  const getMainVideos = useCallback(async () => {
+    try {
+      const res = await axios.get(
+        `/search?part=snippet&maxResults=10&q=beautiful%20place`
+      );
+      let videosArray = await res.data.items;
+      console.log("videosArray 1", videosArray);
+
+      videosArray = await getVideoInfo(videosArray);
+      console.log("videosArray 2", videosArray);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMainVideos();
+  }, [getMainVideos]);
+
+  return <div>MainPage</div>;
+};
+
+export default MainPage;
