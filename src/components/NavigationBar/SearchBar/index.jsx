6d8b4820cb56c0ee10d1bd7d1e3eda5ad6b82141
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-12 00:11:08 +0900

    searchbar 검색 기능 추가

--- src/components/NavigationBar/SearchBar/index.jsx
+++ src/components/NavigationBar/SearchBar/index.jsx
@@ -3,16 +3,50 @@ import { ImSearch } from "react-icons/im";
 import { MdKeyboardVoice } from "react-icons/md";
 import useWindowSize from "../../../helpers/useWindowSize";
 import { SearchContext } from "../../../context/SearchContext";
+import { useNavigate } from "react-router-dom";
+import axios from "../../../api/axios";
 
 const SearchBar = () => {
   const { width } = useWindowSize(); // windowSize 안의 width
-  const { setShowSpecialSearchBar } = useContext(SearchContext);
+  const { searchQuery, setSearchQuery, setShowSpecialSearchBar } =
+    useContext(SearchContext);
+
+  let navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearchQuery({
+      ...searchQuery,
+      input: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (searchQuery.input !== "") {
+      const response = await axios.get(
+        `/search?part=snippet&maxResults=10&q=${searchQuery.input}`
+      );
+
+      setSearchQuery({
+        ...searchQuery,
+        videos: response.data.items,
+      });
+
+      navigate(`/results/${searchQuery.input}`);
+    }
+  };
 
   return (
     <div className={`SearchBar ${width <= 640 ? "smallSearch" : ""}`}>
       {width > 640 ? (
-        <form>
-          <input type="text" name="search" placeholder="Search" />
+        <form onSubmit={handleSubmit}>
+          <input
+            value={searchQuery.input}
+            type="text"
+            name="search"
+            placeholder="Search"
+            onChange={handleChange}
+          />
           <button type="submit">
             <ImSearch
               size={20}
