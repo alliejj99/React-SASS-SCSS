998f1353c60c4003a600a732f4874a4976f75ccc
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-11 22:29:04 +0900

    VideoPage UI 생성 및 스타일 추가

--- src/pages/VideoPage/index.jsx
+++ src/pages/VideoPage/index.jsx
@@ -1,43 +1,183 @@
-import React, { useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import YouTube from "react-youtube";
+import { RiFlagLine, RiShareForwardLine } from "react-icons/ri";
+import { BiLike, BiDislike } from "react-icons/bi";
+import { MdPlaylistAdd } from "react-icons/md";
+// import RelatedVideos from "./RelatedVideos";
+import dayjs from "dayjs";
 import { SideBarContext } from "../../context/SideBarContext";
+import axios from "../../api/axios";
+import relativeTime from "dayjs/plugin/relativeTime";
+// import formatNumber from '../../helpers/formatNumber'
+import formatViews from "../../helpers/formatViews";
+// import formatText from '../../helpers/formatText'
+
+dayjs.extend(relativeTime);
 
 const VideoPage = () => {
-  const { videoId } = useParams(); // router로 설정한 url 값
-  const { setIsToggled } = useContext(SideBarContext);
+  const { videoId } = useParams();
   let location = useLocation();
+  const { state: currentVideo } = location;
+  const { setIsToggled } = useContext(SideBarContext);
+  const [videoComments, setVideoComments] = useState([]);
 
-  const { state: currentVideo } = location; // VideoCard에서 Link를 클릭할때 ...video 정보를 받아서 확인이 가능
+  const loadComments = useCallback(async () => {
+    setIsToggled(false);
+    const response = await axios.get(
+      `/commentThreads?part=snippet&videoId=${videoId}`
+    );
+    setVideoComments(response.data.items);
+  }, [setIsToggled, videoId]);
 
   const opts = {
     width: "640",
     height: "390",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
-  const onPlayReady = (e) => {
+  const onPlayerReady = (e) => {
     e.target.playVideo();
   };
 
+  // const views = formatNumber(currentVideo.extraInfo.viewCount)
+  // const comments = formatNumber(currentVideo.extraInfo.commentCount)
+
+  const likes = formatViews(currentVideo.extraInfo.likeCount);
+  const dislikes = formatViews(currentVideo.extraInfo.dislikeCount);
+  const subscribers = formatViews(currentVideo.channelInfo.subscriberCount);
+
+  // const videoDescription = formatText(currentVideo.snippet.description)
+
+  const videoHeaderMarkUp = (
+    <div className="video_main_info">
+      <div className="tags">
+        {currentVideo?.snippet?.tags?.map((tag, i) => (
+          <p className="tag" key={i}>
+            #{tag}
+          </p>
+        ))}
+      </div>
+      <h1>{currentVideo.snippet.title}</h1>
+      <div className="videoplayer_metadata">
+        <span>{currentVideo.extraInfo.viewCount} views</span>
+        <span className="dot_separator"> &#8226; </span>
+        <span>
+          {dayjs(currentVideo.snippet.publishedAt).format("MMM D, YYYY")}
+        </span>
+      </div>
+    </div>
+  );
+
   useEffect(() => {
-    // 비디오 페이지 처음 들어갔을때 사이드바 안보이게
     setIsToggled(false);
   }, []);
 
+  useEffect(() => {
+    loadComments();
+  }, [loadComments]);
+
+  const videoCommentsMarkUp = videoComments?.map((item) => {
+    const { id, snippet } = item.snippet.topLevelComment;
+    return (
+      <div className="comment_container" key={id}>
+        <div className="comment_avatar_container">
+          <img src={snippet.authorProfileImageUrl} alt="user avatar" />
+        </div>
+        <div className="comment_text_container">
+          <div className="comment_author">
+            {snippet.authorDisplayName}
+            <span>{dayjs(snippet.publishedAt).fromNow()}</span>
+          </div>
+          <div className="comment_text">{snippet.textOriginal}</div>
+          <div className="comment_buttons">
+            <div>
+              <BiLike size={16} />
+              <span className="muted">100</span>
+            </div>
+            <div>
+              <BiDislike size={16} />
+            </div>
+            <span className="muted">REPLY</span>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <React.Fragment>
-      <YouTube
-        className="youtube_player"
-        videoId={videoId}
-        onPlay={onPlayReady}
-        opts={opts}
-        autoplay
-      />
-    </React.Fragment>
+    <section className="videoPage">
+      <div className="columns_container">
+        <div className="column column_1">
+          <div className="youtube_player_container">
+            <YouTube
+              className="youtube_player"
+              videoId={videoId}
+              onPlay={onPlayerReady}
+              opts={opts}
+              autoplay
+            />
+          </div>
+          <div className="videoplayer_info">
+            {videoHeaderMarkUp}
+            <div className="main_header_buttons">
+              <div className="likes_container">
+                <div className="likes">
+                  <BiLike size={25} />
+                  <span>{likes}</span>
+                </div>
+                <div className="dislikes">
+                  <BiDislike size={25} />
+                  <span>{dislikes}</span>
+                </div>
+              </div>
+              <div className="share">
+                <RiShareForwardLine size={25} />
+                <span>SHARE</span>
+              </div>
+              <div className="save">
+                <MdPlaylistAdd size={25} />
+                <span>SAVE</span>
+              </div>
+              <div className="report">
+                <RiFlagLine size={25} className="sidebar_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="channel_video_info">
+            <div className="channel_data">
+              <div className="channel_avatar">
+                <img
+                  src={currentVideo.channelInfo.thumbnails.default.url}
+                  alt="avatar"
+                />
+              </div>
+              <div className="channel_title">
+                <a href="/">{currentVideo.channelInfo.title}</a>
+                <span>{subscribers} subscribers</span>
+              </div>
+              <div className="channel_subscribe">
+                <button>SUBSCRIBED</button>
+              </div>
+            </div>
+            <div className="video_description">
+              {currentVideo.snippet.description}
+            </div>
+          </div>
+          <div className="video_comments_container">
+            <div className="video_comments_count">
+              {currentVideo.extraInfo.commentCount} Comments
+            </div>
+            <div className="video_comments">{videoCommentsMarkUp}</div>
+          </div>
+        </div>
+        <div className="column column_2">
+          {/* <RelatedVideos currentVideo={videoId} /> */}
+        </div>
+      </div>
+    </section>
   );
 };
 
